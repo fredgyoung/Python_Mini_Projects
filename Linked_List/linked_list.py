8cfdd3404edb5c290c8bdb40d4432f16db4adac1
Fred Young <fredgyoung@gmail.com>
2022-10-01 21:54:58 -0400

    renamed variables

--- Linked_List/linked_list.py
+++ Linked_List/linked_list.py
@@ -1,21 +1,27 @@
 
 
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data, next_node=None):
         self.data = data
-        self.next = next
+        self.next = next_node
 
-    def add_next(self, next):
-        self.next = next
+    def add_next(self, next_node):
+        self.next = next_node
 
 
-class LinkedList:
+class SingleLinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
 
-    def add_head(self, node):
-        pass
+    def add_head(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
 
-    def add_tail(self, node):
-        pass
+    def add_tail(self, data):
+        new_node = Node(data)
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
